1574a24143b85674207a0a8413883cd6c2c139bc
Thanh Hai <Phamthanhhai241@gmail.com>
2025-06-22 22:44:54 +0700

    them chuc nang user 3

---
+++ Models/WorkShift.cs
@@ -0,0 +1,10 @@
+﻿namespace WebsiteHotelManagerment.Models
+{
+    public class WorkShift
+    {
+        public int Id { get; set; }
+        public string EmployeeName { get; set; }
+        public DateTime ShiftDate { get; set; }
+        public string ShiftTime { get; set; } // Ví dụ: "Sáng", "Chiều", "Tối"
+    }
+}
